1aa06015efdafceabd0b094251720b6b92856223
Ben Holland <bentheperson1@gmail.com>
2024-04-13 13:49:39 -0500

    intervals start

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from playsound import playsound
 window_width = 1280
 window_height = 960
 
+center_x = window_width / 2
+center_y = window_height / 2
+
 cap = cv2.VideoCapture(0)
 cap.set(3, window_width)
 cap.set(4, window_height)
@@ -18,6 +21,8 @@ sound_play_timer_set = 15
 
 left_sound = "left.wav"
 right_sound = "blip.wav"
+
+interval_size = 320
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
@@ -45,8 +50,6 @@ while True:
 
     sound_play_timer -= 1
 
-    print(sound_play_timer)
-
     for r in results:
         boxes = r.boxes
 
@@ -60,28 +63,33 @@ while True:
                 x_pos = round((x2 + x1) / 2)
                 y_pos = round((y2 + y1) / 2)
 
-                cv2.circle(img, (x_pos, y_pos), 3, (255, 255, 255), -1)
+                if x_pos < center_x + interval_size and x_pos > center_x - interval_size:
+                    if x_pos > center_x:
+                        txt = "Move Left"
 
-                if x_pos > window_width / 2:
-                    txt = "Move Left"
+                        if sound_play_timer <= 0:
+                            sound_play_timer = sound_play_timer_set
 
-                    if sound_play_timer <= 0:
-                        sound_play_timer = sound_play_timer_set
+                            audio_thread = threading.Thread(target=play_left_beep)
+                            audio_thread.start()
+                    else:
+                        txt = "Move Right"
 
-                        audio_thread = threading.Thread(target=play_left_beep)
-                        audio_thread.start()
-                else:
-                    txt = "Move Right"
+                        if sound_play_timer <= 0:
+                            sound_play_timer = sound_play_timer_set
 
-                    if sound_play_timer <= 0:
-                        sound_play_timer = sound_play_timer_set
+                            audio_thread = threading.Thread(target=play_right_beep)
+                            audio_thread.start()
 
-                        audio_thread = threading.Thread(target=play_right_beep)
-                        audio_thread.start()
+                    cv2.circle(img, (x_pos, y_pos), 3, (255, 255, 255), -1)
+                    cv2.putText(img, txt, [32, 32], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (160, 255, 255), 3)
+                    cv2.putText(img, classNames[int(box.cls[0])], [x1, y1], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
 
-                cv2.putText(img, txt, [32, 32], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                cv2.rectangle(img, (x1, y1), (x2, y2), (160, 255, 255), 3)
-                cv2.putText(img, classNames[int(box.cls[0])], [x1, y1], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+    r_line_x = round(center_x + interval_size)
+    l_line_x = round(center_x - interval_size)
+    cv2.line(img, (r_line_x, 0), (r_line_x, window_height), (0,255,0),3)
+    cv2.line(img, (l_line_x, 0), (l_line_x, window_height), (0,255,0),3)
 
     cv2.imshow('Navigation', img)
     if cv2.waitKey(1) == ord('q'):
